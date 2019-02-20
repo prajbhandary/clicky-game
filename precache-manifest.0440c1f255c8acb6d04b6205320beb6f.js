8e3a3b1aaa8a340fe2a9a368bfaacb6a14eefab6
Pranab Rajbhandary <rajbhandaryp@gmail.com>
2019-02-19 22:55:31 -0600

    Updates

---
+++ precache-manifest.0440c1f255c8acb6d04b6205320beb6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "877cb2eacc05719438b1",
+    "url": "/clicky-game/static/js/main.877cb2ea.chunk.js"
+  },
+  {
+    "revision": "16ee49e70560455c2fe9",
+    "url": "/clicky-game/static/js/2.16ee49e7.chunk.js"
+  },
+  {
+    "revision": "877cb2eacc05719438b1",
+    "url": "/clicky-game/static/css/main.7bc87f4b.chunk.css"
+  },
+  {
+    "revision": "b459e5d698bb70ecf4ddb495df6b84bf",
+    "url": "/clicky-game/index.html"
+  }
+];
